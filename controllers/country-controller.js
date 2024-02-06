a6a5f928e3944d2e2a7a0600ccddb9cc2071d8b3
zephania <zephania1656@live.com>
2024-02-06 15:29:30 +0000

    add patch method for countries

--- controllers/country-controller.js
+++ controllers/country-controller.js
@@ -1,36 +1,36 @@
 const Country = require("../models/Country")
 
 const index = async (req, res) => {
-    try{
+    try {
         const data = await Country.getAll()
         res.status(200).json(data)
-    }catch (error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
-} 
+}
 
 const show = async (req, res) => {
-    try{
+    try {
         let name = req.params.name
         const country = await Country.getOneByCountryName(name)
         res.status(200).json(country)
-    }catch (error){
-        res.status(404).json({error: error.message})
+    } catch (error) {
+        res.status(404).json({ error: error.message })
     }
 }
 
 const create = async (req, res) => {
-    try{
+    try {
         const data = req.body
         const newCountry = await Country.create(data)
         res.status(201).json(newCountry)
-    }catch (error){
-        res.status(400).json({error: error.message})
+    } catch (error) {
+        res.status(400).json({ error: error.message })
     }
 }
 
 const destroy = async (req, res) => {
-    try{
+    try {
         //find what we need to delete
         const data = req.params.name
         const country = await Country.getOneByCountryName(data)
@@ -38,9 +38,44 @@ const destroy = async (req, res) => {
         res.status(204).end()
         //delete
 
-    }catch (error){
-        res.status(404).json({error: error.message})
+    } catch (error) {
+        res.status(404).json({ error: error.message })
     }
 }
 
-module.exports = { index, show, create, destroy }
+const update = async (req, res) => {
+    try {
+        const data = req.params.name
+        const newData = req.body
+        const country = await Country.getOneByCountryName(data)
+        console.log(country, newData.capital)
+        const updatedData = {}
+        if (req.body.name === undefined) {
+            updatedData.name = country.name
+        } else {
+            updatedData.name = newData.name
+        }
+        if (req.body.capital === undefined) {
+            updatedData.capital = country.capital
+        } else {
+            updatedData.capital = newData.capital
+        }
+        if (req.body.population === undefined) {
+            updatedData.population = country.population
+        } else {
+            updatedData.population = newData.population
+        }
+        if (req.body.languages === undefined) {
+            updatedData.languages = country.languages
+        } else {
+            updatedData.languages = newData.languages
+        }
+        const result = await country.update(updatedData)
+        res.status(200).json(result)
+
+    } catch (error) {
+        res.status(404).json({ error: error.message })
+    }
+}
+
+module.exports = { index, show, create, destroy, update }
--- models/Country.js
+++ models/Country.js
@@ -1,38 +1,38 @@
 const db = require("../database/connect")
 
 class Country {
-    constructor({country_id, name, capital, population, languages, fun_fact, map_image_url}) {
-      this.country_id = country_id
-      this.name = name
-      this.capital = capital
-      this.population = population
-      this.languages = languages
-      this.fun_fact = fun_fact
-      this.map_image_url = map_image_url
+    constructor({ country_id, name, capital, population, languages, fun_fact, map_image_url }) {
+        this.country_id = country_id
+        this.name = name
+        this.capital = capital
+        this.population = population
+        this.languages = languages
+        this.fun_fact = fun_fact
+        this.map_image_url = map_image_url
     }
 
     static async getAll() {
         const response = await db.query("SELECT name FROM country;")
-        if (response.rows.length === 0){
+        if (response.rows.length === 0) {
             throw new Error("No countries in the database")
         }
 
         return response.rows.map(c => new Country(c))
     }
 
-    static async getOneByCountryName(cn)  {
+    static async getOneByCountryName(cn) {
         const response = await db.query("SELECT * FROM country WHERE LOWER(name) = LOWER($1);", [cn])
-        if(response.rows.length != 1){
+        if (response.rows.length != 1) {
             throw new Error("Can not get country")
         }
         return new Country(response.rows[0])
     }
 
     static async create(data) {
-        const {name, capital, population, languages} = data
-        const ec = await db.query("SELECT name FROM country WHERE LOWER(name) = LOWER($1);",[name])
-        
-        if(ec.rows.length > 0){
+        const { name, capital, population, languages } = data
+        const ec = await db.query("SELECT name FROM country WHERE LOWER(name) = LOWER($1);", [name])
+
+        if (ec.rows.length > 0) {
             throw new Error("Country already is in the DB")
         }
 
@@ -40,14 +40,24 @@ class Country {
         return new Country(response.rows[0])
     }
 
-    async destroy(){
+    async destroy() {
         let response = await db.query("DELETE FROM country WHERE name = $1 RETURNING *;", [this.name])
         return new Country(response.rows[0])
     }
 
-    
+    async update(data) {
+        const { name, capital, population, languages, fun_fact, map_image_url } = data
+        const ec = await db.query("SELECT name FROM country WHERE LOWER(name) = LOWER($1);", [name])
+        if (ec.rows.length = 0) {
+            throw new Error("Country not in the DB")
+        }
+        const response = await db.query(
+            "UPDATE country SET name = $1, capital = $2, population = $3, languages = $4, fun_fact = $5, map_image_url = $6 WHERE name = $7 RETURNING *;",
+            [name, capital, population, languages, fun_fact, map_image_url, this.name]
+        )
+        return new Country(response.rows[0])
+    }
 
+}
 
-  }
-  
-  module.exports = Country;
+module.exports = Country;
